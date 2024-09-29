5898c7ff160a0b7a0fe9269eef7eda0d855fb12c
Justin Bennett <git@just-be.dev>
2024-09-28 23:09:23 -0400

    Update examples w/ origin usage; persistence

--- examples/load-html.ts
+++ examples/load-html.ts
@@ -3,9 +3,16 @@ import { createWebView } from "../src/lib.ts";
 using webview = await createWebView({
   title: "Load Html Example",
   html: "<h1>Initial html</h1>",
+  // Note: This origin is used with a custom protocol so it doesn't match
+  // https://example.com. This doesn't need to be set, but can be useful if
+  // you want to control resources that are scoped to a specific origin like
+  // local storage or indexeddb.
+  origin: "example.com",
+  devtools: true,
 });
 
 webview.on("started", async () => {
+  await webview.openDevTools();
   await webview.loadHtml("<h1>Updated html!</h1>");
 });
 
