5e2028f6333dd49ede91fa0a8d2bfa56c4408f6b
Justin Bennett <git@just-be.dev>
2025-02-06 22:20:31 -0500

    Add remaining examples

---
+++ src/clients/python/examples/load_html.py
@@ -0,0 +1,36 @@
+import sys
+import asyncio
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).parent.parent))
+from main import WebView, WebViewOptions, WebViewContentHtml, WebViewNotification
+
+
+async def main():
+    print("Creating webview")
+    config = WebViewOptions(
+        title="Load Html Example",
+        load=WebViewContentHtml(
+            html="<h1>Initial html</h1>",
+            # Note: This origin is used with a custom protocol so it doesn't match
+            # https://example.com. This doesn't need to be set, but can be useful if
+            # you want to control resources that are scoped to a specific origin like
+            # local storage or indexeddb.
+            origin="example.com",
+        ),
+        devtools=True,
+    )
+
+    async with WebView(config) as webview:
+
+        async def handle_start(event: WebViewNotification):
+            await webview.open_devtools()
+            await webview.load_html("<h1>Updated html!</h1>")
+
+        webview.on("started", handle_start)
+
+    print("Webview closed")
+
+
+if __name__ == "__main__":
+    asyncio.run(main()) 
